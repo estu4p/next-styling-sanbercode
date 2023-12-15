d5150f1962220a93ecdbc3872e09ffab2e955d0d
estu <rezapramudya112@gmail.com>
2023-12-15 08:43:35 +0700

    tugas hari-4

---
+++ components/footer/index.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="bg-black h-16 text-center">
+      <p className="text-white text-xl font-bold">Footer</p>
+    </div>
+  );
+}
---
+++ components/header/index.js
@@ -0,0 +1,11 @@
+import style from "./styles.module.css";
+
+function Header() {
+  return (
+    <div className={style.header}>
+      <p className={style.header_content}>Footer</p>
+    </div>
+  );
+}
+
+export default Header;
